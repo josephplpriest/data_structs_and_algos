583e7fc3df404b8675b982393657aa6acd14548c
Paul Priest <josephppriest@gmail.com>
2023-08-05 09:42:27 -0400

    init src for contains_dups

---
+++ contains_duplicates/src/main.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+fn naive_duplicates(nums: Vec<i32>) -> bool {
+    
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+         let mut int_counts: HashMap<i32, i32> = HashMap::new();
+    }
+}
